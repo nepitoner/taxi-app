59168d2d0256598b7c78dbd51ccf9f7eedd02e03
Oksana Shik <shikoksana57@gmail.com>
2025-01-24 15:48:13 +0300

    add file validator

---
+++ passenger-service/src/main/java/org/example/utils/validator/annotation/NotEmptyFile.java
@@ -0,0 +1,25 @@
+package org.example.utils.validator.annotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import org.example.utils.validator.FileEmptinessValidator;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static org.example.utils.constant.ExceptionConstant.EMPTY_FILE_MESSAGE;
+
+@Constraint(validatedBy = FileEmptinessValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NotEmptyFile {
+
+    String message() default EMPTY_FILE_MESSAGE;
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
