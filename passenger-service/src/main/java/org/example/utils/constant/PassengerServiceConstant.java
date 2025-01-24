b3056029ef1e7c75c51ac070033b77bc8c0dcbd4
Oksana Shik <shikoksana57@gmail.com>
2025-01-24 15:49:51 +0300

    add constants

---
+++ passenger-service/src/main/java/org/example/utils/constant/PassengerServiceConstant.java
@@ -0,0 +1,12 @@
+package org.example.utils.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PassengerServiceConstant {
+
+    public final static String INCORRECT_DATA_MESSAGE = "Incorrect data";
+
+    public final static String PHONE_NUMBER_REGEX = "^\\+375(29|25|33|44)\\d{7}$";
+
+}
