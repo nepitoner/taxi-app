e02a56cd2c53d6f3772d31bc78929e9b9aaf7338
Oksana Shik <shikoksana57@gmail.com>
2025-01-24 15:46:36 +0300

    add configs

---
+++ passenger-service/src/main/java/org/example/config/ClockConfig.java
@@ -0,0 +1,14 @@
+package org.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
+
+@Configuration
+public class ClockConfig {
+    @Bean
+    Clock clock() {
+        return Clock.systemDefaultZone();
+    }
+}
