4b49e85b67513be9355b253158fecddf2014db18
Oksana Shik <shikoksana57@gmail.com>
2025-01-25 20:34:25 +0300

    edit mapping

--- passenger-service/src/main/java/org/example/mapper/PassengerMapper.java
+++ passenger-service/src/main/java/org/example/mapper/PassengerMapper.java
@@ -1,35 +1,32 @@
 package org.example.mapper;
 
+import org.example.dto.PagedPassengerResponse;
 import org.example.dto.PassengerRequest;
 import org.example.dto.PassengerResponse;
 import org.example.entity.Passenger;
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        builder = @Builder(disableBuilder = true),
+        componentModel = MappingConstants.ComponentModel.SPRING
+)
 public interface PassengerMapper {
 
-    @Mapping(target = "rating", ignore = true)
-    @Mapping(target = "passengerId", ignore = true)
-    @Mapping(target = "isDeleted", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
-    @Mapping(target = "profilePictureRef", ignore = true)
     Passenger mapDtoToEntity(PassengerRequest passengerRequest);
 
-    @Mapping(target = "rating", ignore = true)
-    @Mapping(target = "isDeleted", ignore = true)
-    @Mapping(target = "profilePictureRef", ignore = true)
     Passenger mapDtoToEntity(PassengerRequest passengerRequest, UUID passengerId,
                              LocalDateTime createdAt, LocalDateTime updatedAt);
 
-    @Mapping(target = "totalPageAmount", ignore = true)
     PassengerResponse mapEntityToDto(Passenger passenger);
 
-    PassengerResponse mapEntityToDto(Passenger passenger, int totalPageAmount);
+    PagedPassengerResponse mapPageEntityToPagedDto(int page, int limit, long totalPassengers, List<PassengerResponse> passengers);
 
 }
