f53ae839de029b11dd1f218fa0e496eb59a0f841
Oksana Shik <shikoksana57@gmail.com>
2025-01-25 20:32:10 +0300

    add constant

--- passenger-service/src/main/java/org/example/utils/constant/ExceptionConstant.java
+++ passenger-service/src/main/java/org/example/utils/constant/ExceptionConstant.java
@@ -5,10 +5,14 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class ExceptionConstant {
 
-    public final static String NOT_FOUND_MESSAGE = "Passenger with id %s not found";
+    public final static String PASSENGER_NOT_FOUND_MESSAGE = "Passenger with id %s not found";
 
-    public final static String REPEATED_DATA_MESSAGE = "Passenger with phone %s or email %s already exists";
+    public final static String REPEATED_EMAIL_MESSAGE = "Passenger with email %s already exists";
+
+    public final static String REPEATED_PHONE_NUMBER_MESSAGE = "Passenger with phone number %s already exists";
 
     public final static String EMPTY_FILE_MESSAGE = "File must not be empty";
+
+    public final static String INCORRECT_DATA_MESSAGE = "Incorrect data";
 
 }
