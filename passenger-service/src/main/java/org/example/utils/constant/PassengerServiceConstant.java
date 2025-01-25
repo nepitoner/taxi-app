ec935916e17c93a91de0e156f6926cc8f69ac42e
Oksana Shik <shikoksana57@gmail.com>
2025-01-25 20:32:51 +0300

    remove constant

--- passenger-service/src/main/java/org/example/utils/constant/PassengerServiceConstant.java
+++ passenger-service/src/main/java/org/example/utils/constant/PassengerServiceConstant.java
@@ -5,8 +5,6 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class PassengerServiceConstant {
 
-    public final static String INCORRECT_DATA_MESSAGE = "Incorrect data";
-
     public final static String PHONE_NUMBER_REGEX = "^\\+375(29|25|33|44)\\d{7}$";
 
 }
