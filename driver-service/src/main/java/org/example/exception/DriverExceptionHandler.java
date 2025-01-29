e8f365c2d3203b0cc9c5f0d3985edef7d5d3a006
Oksana Shik <shikoksana57@gmail.com>
2025-01-29 23:45:19 +0300

    add exceptions

---
+++ driver-service/src/main/java/org/example/exception/DriverExceptionHandler.java
@@ -0,0 +1,57 @@
+package org.example.exception;
+
+import jakarta.validation.ConstraintViolationException;
+import org.example.dto.exception.ErrorResponse;
+import org.example.dto.exception.ValidationErrorResponse;
+import org.example.dto.exception.Violation;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class DriverExceptionHandler {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({DriverNotFoundException.class, CarNotFoundException.class})
+    public ErrorResponse handleNotFoundException(Exception exception) {
+        return new ErrorResponse(HttpStatus.NOT_FOUND, exception.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(RepeatedDataException.class)
+    ErrorResponse handleRepeatedDataException(RepeatedDataException exception) {
+        return new ErrorResponse(HttpStatus.CONFLICT, exception.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(RequestTimeoutException.class)
+    ErrorResponse handleBadRequestException(RequestTimeoutException exception) {
+        return new ErrorResponse(HttpStatus.BAD_REQUEST, exception.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    ValidationErrorResponse handleConstraintViolationException(ConstraintViolationException ex) {
+        final List<Violation> violations = ex.getConstraintViolations().stream()
+                .map(violation -> new Violation(
+                        violation.getPropertyPath().toString(),
+                        violation.getMessage()))
+                .collect(Collectors.toList());
+        return new ValidationErrorResponse(violations);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ValidationErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        final List<Violation> violations = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> new Violation(error.getField(), error.getDefaultMessage()))
+                .collect(Collectors.toList());
+        return new ValidationErrorResponse(violations);
+    }
+
+}
---
+++ driver-service/src/main/java/org/example/exception/DriverNotFoundException.java
@@ -0,0 +1,7 @@
+package org.example.exception;
+
+public class DriverNotFoundException extends RuntimeException {
+    public DriverNotFoundException(String message) {
+        super(message);
+    }
+}
