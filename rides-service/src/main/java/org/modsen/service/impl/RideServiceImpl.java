e1df30b2f13a8fb214e19a8988f493e20e4a81e2
Oksana Shik <shikoksana57@gmail.com>
2025-02-19 12:19:21 +0300

    add ride creating with passenger check and driver notify

--- rides-service/src/main/java/org/modsen/service/impl/RideServiceImpl.java
+++ rides-service/src/main/java/org/modsen/service/impl/RideServiceImpl.java
@@ -2,9 +2,9 @@ package org.modsen.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.modsen.client.DriverClient;
 import org.modsen.client.PassengerClient;
 import org.modsen.dto.request.RideAvailableEvent;
+import org.modsen.client.DriverClient;
 import org.modsen.dto.request.RideRequest;
 import org.modsen.dto.request.RideRequestParams;
 import org.modsen.dto.request.RideStatusRequest;
